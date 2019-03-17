731ef97cb5adccac8a8c83502444ba5a58733035
Vladislav Velmisov <velmisovx@gmail.com>
2019-03-17 15:29:25 +0300

    add weight to Edge

--- Edge.py
+++ Edge.py
@@ -1,9 +1,10 @@
 
 class Edge:
-    def __init__(self, _id, out_of, to):
+    def __init__(self, _id, out_of, to, weight=None):
         self.id = _id
         self.out_of = out_of
         self.to = to
+        self.weight = weight or 1
 
     def __str__(self):
         return "Edge. id: '" + self.id + "', out of: '" + self.out_of + "', to: '" + self.to + "'"
