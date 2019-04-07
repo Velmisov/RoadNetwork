d08b5d602e869438ae55814d0343e1207d7600af
Vladislav Velmisov <velmisovx@gmail.com>
2019-04-07 15:15:17 +0300

    test weight with turns

---
+++ experiments/wturns.py
@@ -0,0 +1,36 @@
+import sys
+import traci
+import subprocess
+from models.RoadNetwork import RoadNetwork
+from models.parser import parse
+from models.Vehicle import Vehicle
+
+# added by File -> Settings -> Project interpreter -> Chosen interpreter -> add to path: /usr/share/sumo/tools
+# if 'SUMO_HOME' in os.environ:
+#     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+#     sys.path.append(tools)
+# else:
+#     sys.exit("please declare environment variable 'SUMO_HOME'")
+
+port = 10080
+sumoCmd = ['sumo-gui', '-c', '../data/weight_with_turns/wturns.sumocfg', '--remote-port', str(port)]
+
+sumoProcess = subprocess.Popen(sumoCmd, stdout=sys.stdout, stderr=sys.stderr)
+traci.init(port)
+
+edges = parse('../data/weight_with_turns/wturns.net.xml')
+
+rn = RoadNetwork(edges)
+vehicles = {}
+while not rn.empty():
+    rn.simulation_step()
+    active_vehicles = traci.vehicle.getIDList()
+    for vehicle_id in active_vehicles:
+        if vehicle_id not in vehicles:
+            vehicles[vehicle_id] = Vehicle(vehicle_id)
+            route = rn.Deijkstra(vehicle_id, rn.weight_with_turns)
+            print(route)
+            traci.vehicle.setRoute(vehicle_id, route)
+
+traci.close()
+sumoProcess.terminate()
