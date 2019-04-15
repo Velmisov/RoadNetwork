dcd05b17013f5a1f7cd23047fda79294883ce5e7
Vladislav Velmisov <velmisovx@gmail.com>
2019-04-15 15:10:03 +0300

    special vehicle easy test

---
+++ experiments/sveasy.py
@@ -0,0 +1,40 @@
+import sys
+import traci
+import subprocess
+from models.RoadNetwork import RoadNetwork
+from models.parser import parse
+from models.Vehicle import Vehicle
+
+# added by File -> Settings -> Project interpreter -> Chosen interpreter -> add to path: /usr/share/sumo/tools
+# if 'SUMO_HOME' in os.environ:
+#     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+#     sys.path.append(tools)
+# else:
+#     sys.exit("please declare environment variable 'SUMO_HOME'")
+
+port = 10080
+sumoCmd = ['sumo-gui', '-c', '../data/special_vehicle_easy/sveasy.sumocfg', '--remote-port', str(port)]
+
+sumoProcess = subprocess.Popen(sumoCmd, stdout=sys.stdout, stderr=sys.stderr)
+traci.init(port)
+
+edges = parse('../data/special_vehicle_easy/sveasy.net.xml')
+
+rn = RoadNetwork(edges, 'specialCar')
+vehicles = {}
+while not rn.empty():
+    rn.simulation_step(green_for_special_car=True)
+    # print(traci.trafficlight.getCompleteRedYellowGreenDefinition('center'))
+    # print(traci.trafficlight.getProgram('center'))
+    # print(traci.trafficlight.getControlledLanes('center'))
+    # print(traci.trafficlight.getRedYellowGreenState('center'))
+    # active_vehicles = traci.vehicle.getIDList()
+    # for vehicle_id in active_vehicles:
+    #     if vehicle_id not in vehicles:
+    #         vehicles[vehicle_id] = Vehicle(vehicle_id)
+    #         route = rn.Deijkstra(vehicle_id, rn.weight_with_own_speed)
+    #         print(route)
+    #         traci.vehicle.setRoute(vehicle_id, route)
+
+traci.close()
+sumoProcess.terminate()
