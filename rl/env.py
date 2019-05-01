cda8d8656c97c8b4088bf6547db1a9652d471ac9
Vladislav Velmisov <velmisovx@gmail.com>
2019-05-01 12:18:06 +0300

    add env

---
+++ rl/env.py
@@ -0,0 +1,26 @@
+import gym
+from gym import spaces
+
+
+class SumoEnv(gym.Env):
+    """Custom Environment that follows gym interface"""
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self):
+        super(SumoEnv, self).__init__()
+        # Define action and observation space
+        # They must be gym.spaces objects
+        # Example when using discrete actions:
+        self.action_space = spaces.Discrete(N_DISCRETE_ACTIONS)
+        # Example for using image as input:
+        self.observation_space = spaces.Box(low=0, high=255,
+                                            shape=(HEIGHT, WIDTH, N_CHANNELS), dtype=np.uint8)
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self, mode='human', close=False):
+        pass
