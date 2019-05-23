86860788533683087ef8900e342130a397f15aec
Vladislav Velmisov <velmisovx@gmail.com>
2019-05-23 16:44:28 +0300

    test rl model

---
+++ rl/test.py
@@ -0,0 +1,35 @@
+from rl.env import SumoEnv
+from rl.gen_route import write_route_file
+import traci
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import SubprocVecEnv
+from stable_baselines import A2C
+
+# write_route_file('./nets/single-intersection-gen.rou.xml', 400000, 100000)
+
+sumo_env = SumoEnv(net_file='../data/default/default.net.xml',
+                   route_file='../data/default/default.rou.xml',
+                   out_csv_name='../data/theory/model-little.csv',
+                   gui=True,
+                   num_seconds=400000,
+                   min_green=5,
+                   phases=[
+                       traci.trafficlight.Phase(32000, "GGrrrrGGrrrr"),
+                       traci.trafficlight.Phase(2000, "yyrrrryyrrrr"),
+                       traci.trafficlight.Phase(32000, "rrGrrrrrGrrr"),
+                       traci.trafficlight.Phase(2000, "rryrrrrryrrr"),
+                       traci.trafficlight.Phase(32000, "rrrGGrrrrGGr"),
+                       traci.trafficlight.Phase(2000, "rrryyrrrryyr"),
+                       traci.trafficlight.Phase(32000, "rrrrrGrrrrrG"),
+                       traci.trafficlight.Phase(2000, "rrrrryrrrrry")
+                   ])
+
+env = SubprocVecEnv([lambda: sumo_env])
+
+# model = A2C(MlpPolicy, env, verbose=1, learning_rate=0.0001, lr_schedule='constant')
+# model.learn(total_timesteps=50000)
+# model.save('model3')
+
+model = A2C.load('model2')
+sumo_env.check(model)
